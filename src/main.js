5570f41777dbdf0b35bd84be81ebb7cefb3e3579
Barbara Rolon <barbara.rolon.rivero@gmail.com>
2024-08-12 22:40:21 -0500

    Api Key - Open Modal dialog

--- src/main.js
+++ src/main.js
@@ -77,6 +77,30 @@ sort.addEventListener("change", (event) => {
   root.appendChild(renderItems(orderData));
 });
 
+const dialog = document.getElementById('dialog');
+const jsbutton = document.getElementById('jsbutton');
+const modal = document.getElementById('modal');
+const reset = document.getElementById('reset');
+
+modal.addEventListener('click', (event) => {
+  dialog.showModal();
+  text.textContent = '';
+});
+
+jsbutton.addEventListener('click', (event) => {
+  dialog.close();
+  text.innerHTML += '"JS close" closed the dialog.<br/>';
+});
+
+
+dialog.addEventListener('cancel', (event) => {
+  text.innerHTML += 'cance event happened<br/>';
+});
+
+dialog.addEventListener('close', (event) => {
+  text.innerHTML += 'close event happened<br/>';
+});
+
 const clear = document.querySelector("#reset-button"); // Reemplazado getElementById con querySelector
 clear.addEventListener("click", function () {
   filterType.value = "";
